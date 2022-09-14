39af83b8602f006bdd4f2a67464033d0af450c57
F-Arzuaga <108815637+F-Arzuaga@users.noreply.github.com>
2022-09-14 14:03:14 -0300

    stock added

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const productsRouter = express.Router();
 const productController = require("../controllers/productController");
+const { expressjwt: jwt } = require("express-jwt");
+
 // Rutas Públicas:
 // To Do:
 productsRouter.get("/", productController.index);
 productsRouter.get("/:slug", productController.show);
 productsRouter.get("/random/:id", productController.random);
+productsRouter.patch(
+  "/",
+  jwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }),
+  productController.update,
+);
 
 module.exports = productsRouter;
