b05c2296116c73598ab2520514e6b58cfd44c3e7
Sofia Valdez <valdezsofia7@gmail.com>
2022-09-15 16:45:11 -0300

    Rutas y controllers de editar productos

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,11 +1,15 @@
-const { Admin, Order } = require("../models");
+const { Admin, Order, Product, Category } = require("../models");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 // Display a listing of the resource.
-async function index(req, res) {
+async function indexOrders(req, res) {
   const orders = await Order.findAll();
   res.json({ orders });
+}
+async function indexProducts(req, res) {
+  const products = await Product.findAll({ include: Category });
+  res.json({ products });
 }
 
 // Display the specified resource.
@@ -43,12 +47,25 @@ async function login(req, res) {
 }
 
 // Update the specified resource in storage.
-async function update(req, res) {
+async function updateOrder(req, res) {
   console.log(req.body);
   const order = await Order.update({ state: req.body.state }, { where: { id: req.body.id } });
   res.json({ order });
 }
-
+async function updateProducts(req, res) {
+  console.log("OK");
+  console.log(req.body);
+  const isPopular = req.body.data.popular === "true";
+  await Product.update(
+    {
+      ...req.body.data,
+      categoryId: Number(req.body.data.category),
+      popular: isPopular,
+      slug: req.body.data.name,
+    },
+    { where: { id: req.body.id } },
+  );
+}
 // Remove the specified resource from storage.
 async function destroy(req, res) {}
 
@@ -56,10 +73,12 @@ async function destroy(req, res) {}
 // ...
 
 module.exports = {
-  index,
+  indexOrders,
+  indexProducts,
   show,
   store,
   login,
-  update,
+  updateOrder,
+  updateProducts,
   destroy,
 };
