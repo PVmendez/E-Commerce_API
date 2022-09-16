948d732cd1f8c1774a4f2159f72ce2ee8e69b372
Sofia Valdez <valdezsofia7@gmail.com>
2022-09-16 10:22:50 -0300

    storeProducts

--- routes/administratorsRoutes.js
+++ routes/administratorsRoutes.js
@@ -5,14 +5,17 @@ const adminController = require("../controllers/adminController");
 // Rutas del Admin:
 // ...
 
-administratorsRouter.delete("/delete/:id", adminController.destroy);
+administratorsRouter.delete("/delete/:id", adminController.destroyAdmins);
 administratorsRouter.patch("/update/:id", adminController.update);
 administratorsRouter.get("/orders", adminController.indexOrders);
 administratorsRouter.patch("/orders", adminController.updateOrder);
-administratorsRouter.post("/register", adminController.store);
+administratorsRouter.post("/register", adminController.storeAdmins);
 administratorsRouter.post("/login", adminController.login);
 administratorsRouter.get("/products", adminController.indexProducts);
 administratorsRouter.patch("/products", adminController.updateProducts);
+administratorsRouter.delete("/products/:id", adminController.destroyProducts);
+administratorsRouter.post("/products", adminController.storeProducts);
 administratorsRouter.get("/", adminController.index);
 administratorsRouter.get("/:id", adminController.show);
+
 module.exports = administratorsRouter;
