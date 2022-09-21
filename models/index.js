51b924492cefd5c41dc213b6e4aa7339f12f2a8d
Sofia Valdez <valdezsofia7@gmail.com>
2022-09-21 16:27:11 -0300

    pg en index Models

--- models/index.js
+++ models/index.js
@@ -6,7 +6,8 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD, // Ej: root
   {
     host: process.env.DB_HOST, // Ej: 127.0.0.1
-    dialect: process.env.DB_CONNECTION, // Ej: mysql
+    dialect: process.env.DB_CONNECTION,
+    dialectModule: require("pg"),
     logging: false, // Para que no aparezcan mensajes en consola.
   },
 );
