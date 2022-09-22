17c89f200176daa48a07dd8d9d8615b45f9b0f1c
PVmendez <valentino.mendez.rey@gmail.com>
2022-09-22 15:59:14 -0300

    order uses

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const APP_PORT = process.env.APP_PORT || 8000;
 const app = express();
 const cors = require("cors");
 
+app.use(cors());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
 
 routes(app);
 
